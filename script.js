52ff4d7db214468eedb3e33a6426471654c1edbf
Guilherme Menrraz <65176106+Menrraz@users.noreply.github.com>
2022-08-30 14:35:44 -0300

    Adicionar tag div para centralizar elementos

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ function addItem() {
     const main = document.querySelector('main')
     main.insertAdjacentHTML('beforeend', `
     <section id='addItemWindow'>
-        <label>Adicione um item</label>
-        <input type="text" name="NomeDoItem" id="itemName">
-        <label>Quantidade</label>
-        <select>
-            <option value="unity">Unidade</option>
-            <option value="kg">Quilo</option>
-        </select>
-        <input type="number" name="quantidade" id="itemQuantity">
-        <input type="button" value="Adicionar">
-        <p id='warning'>Preencha todos os dados.</p>
+        <div class='centralizar'>
+            <label>Adicione um item</label>
+            <input type="text" name="NomeDoItem" id="itemName">
+            <label>Quantidade</label>
+            <select>
+                <option value="unity">Unidade</option>
+                <option value="kg">Quilo</option>
+            </select>
+            <input type="number" name="quantidade" id="itemQuantity">
+            <input type="button" value="Adicionar">
+            <p id='warning'>Preencha todos os dados.</p>
+        </div>
     </section>`)
     const addItemWindow = document.getElementById('addItemWindow')
     addItemWindow.style.display = 'block'
